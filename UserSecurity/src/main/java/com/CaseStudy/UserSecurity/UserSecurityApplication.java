5d3f38614adb1bfd083bf3e2f57b3fb701d065ec
rugawade <rugawade@LIN25008032.corp.capgemini.com>
2021-12-31 23:35:14 +0530

    Adding All files of User Security

---
+++ UserSecurity/src/main/java/com/CaseStudy/UserSecurity/UserSecurityApplication.java
@@ -0,0 +1,24 @@
+package com.CaseStudy.UserSecurity;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class UserSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserSecurityApplication.class, args);
+	}
+	@Bean
+	public RestTemplate restTemplate()
+	{
+		return new RestTemplate();
+	}
+
+	
+}
